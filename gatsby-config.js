83ab4324e966b56a62276394b35ed81b4b284af3
zizate <giwook5600@naver.com>
2022-07-14 12:14:50 +0900

    gatsby init

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Customize your site metadata:
+  siteMetadata: {
+    pathPrefix: "/litus",
+    title: `My Blog Title`,
+    author: `My Name`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+}
